f1ae2ab19290b959ea8a4abdec46d56ebb29e846
joulecpa <joulecpa@gmail.com>
2015-02-24 23:10:04 -0500

    Updated tester file

--- Assignments/A1/a1q2main.cpp
+++ Assignments/A1/a1q2main.cpp
@@ -11,6 +11,8 @@
 /*   To compile:                        */
 /*   g++ a1q2main.cpp                   */
 /*                                      */
+/*   version 1.1: bug fix to moving     */
+/*   copytoarray out of if statement    */
 /****************************************/
 #include "recentlist.h"
 #include <cstring>
@@ -55,7 +57,7 @@ int main(void){
 	Record data[20]={
 		{"the",1},
 		{"quick",2},
-		{"brown",3},
+		{"brown ",3},
 		{"fox",4},
 		{"jumped",5},
 		{"over",6},
@@ -122,25 +124,25 @@ int main(void){
       numPassed++;
     	cout << "test 3: perform some searches, ensure adjustment is handled" << endl;
    		for(int i=0;i<numSearches;i++){
-
    			int pick=rand()%20;
    			if(list.search(mirror[pick],compareWords)){
      			adjust(pick,mirror);
-     			if(list.copyToArray(check,20) && !cmpArrays(mirror,check,20,compareWords)){
-            passtest=false;
+          list.copyToArray(check,20);
+     			if(!cmpArrays(mirror,check,20,compareWords)){
+   	  			passtest=false;
             cout << "Error 3a: Bug in search." << endl;
    			  }
         }
         else{
           passtest=false;
         }
-
    		}
    		for(int i=0;i<numSearches;i++){
    			int pick=rand()%cap;
    			if(intList.search(intMirror[pick],compareNumbers)){
      			adjust(pick,intMirror);
-     			if(intList.copyToArray(intCheck,cap) && !cmpArrays(intMirror,intCheck,cap,compareNumbers)){
+          intList.copyToArray(intCheck,cap);
+     			if(!cmpArrays(intMirror,intCheck,cap,compareNumbers)){
    	  			passtest=false;
    		 		  cout << "Error 3b: Bug in search."<< endl;
    			  }
@@ -153,11 +155,13 @@ int main(void){
     if(passtest){
       numPassed++;
     	cout << "test 4: ensure that copied list were not altered (proper deep copy was made) " << endl;
-    	if(copy.copyToArray(check,20) && !cmpArrays(data,check,20,compareWords)){
+      copy.copyToArray(check,20);
+    	if(!cmpArrays(data,check,20,compareWords)){
     		passtest=false;
     		cout << "Error 4a: Bug in copy constructor, deep copy not made?"  << endl;  		
     	}
-    	if(intCopy.copyToArray(intCheck,cap) && !cmpArrays(intData,intCheck,cap,compareNumbers)){
+      intCopy.copyToArray(intCheck,cap);
+    	if(!cmpArrays(intData,intCheck,cap,compareNumbers)){
     		passtest=false;
     		cout << "Error 4b: Bug in copy constructor, deep copy not made?" << endl;
     	}
@@ -167,11 +171,13 @@ int main(void){
     	cout << "test 5: test assignment operator"  << endl;
     	copy = list;
     	intCopy = intList;
-    	if(copy.copyToArray(check,20) && !cmpArrays(mirror,check,20,compareWords)){
+      copy.copyToArray(check,20);
+    	if(!cmpArrays(mirror,check,20,compareWords)){
     		passtest=false;
     		cout << "Error 5a: Bug in = operator"  << endl;  		
     	}
-    	if(intCopy.copyToArray(intCheck,cap) && !cmpArrays(intMirror,intCheck,cap,compareNumbers)){
+      intCopy.copyToArray(intCheck,cap);
+    	if(!cmpArrays(intMirror,intCheck,cap,compareNumbers)){
     		passtest=false;
     		cout << "Error 5b: Bug in = operator"  << endl;  		
     	}
@@ -187,13 +193,15 @@ int main(void){
     	cout << "test 6: test remove function"  << endl;
     	list.remove(mirror[19],compareWords);
     	removeItem(mirror,19,20);
-   		if(list.copyToArray(check,20) && !cmpArrays(mirror,check,19,compareWords)){
+      list.copyToArray(check,20);
+   		if(!cmpArrays(mirror,check,19,compareWords)){
    			passtest=false;
    			cout << "Error 6a: Bug in remove." << endl;
    		}
    		list.remove(mirror[0],compareWords);
    		removeItem(mirror,0,19);
-   		if(list.copyToArray(check,20) && !cmpArrays(mirror,check,18,compareWords)){
+      list.copyToArray(check,20);
+   		if(!cmpArrays(mirror,check,18,compareWords)){
    			passtest=false;
    			cout << "Error 6b: Bug in remove." << endl;
    		}
@@ -209,25 +217,29 @@ int main(void){
    		}
     	intList.remove(intMirror[0],compareNumbers);
     	removeItem(intMirror,0,cap);
-   		if(intList.copyToArray(intCheck,cap) && !cmpArrays(intMirror,intCheck,cap-1,compareNumbers)){
+      intList.copyToArray(intCheck,cap);
+   		if(!cmpArrays(intMirror,intCheck,cap-1,compareNumbers)){
    			passtest=false;
    			cout << "Error 6d: Bug in remove." << endl;
    		}
       intList.remove(intMirror[0],compareNumbers);
       removeItem(intMirror,0,cap-1);
-      if(intList.copyToArray(intCheck,cap) && !cmpArrays(intMirror,intCheck,cap-2,compareNumbers)){
+      intList.copyToArray(intCheck,cap);
+      if(!cmpArrays(intMirror,intCheck,cap-2,compareNumbers)){
         passtest=false;
         cout << "Error 6e: Bug in remove." << endl;
       }
       intList.remove(intMirror[cap-3],compareNumbers);
       removeItem(intMirror,cap-3,cap-2);
-      if(intList.copyToArray(intCheck,cap) && !cmpArrays(intMirror,intCheck,cap-3,compareNumbers)){
+      intList.copyToArray(intCheck,cap);
+      if(!cmpArrays(intMirror,intCheck,cap-3,compareNumbers)){
         passtest=false;
         cout << "Error 6f: Bug in remove." << endl;
       }
       intList.remove(intMirror[cap-4],compareNumbers);
       removeItem(intMirror,cap-4,cap-3);
-      if(intList.copyToArray(intCheck,cap) && !cmpArrays(intMirror,intCheck,cap-4,compareNumbers)){
+      intList.copyToArray(intCheck,cap);
+      if(!cmpArrays(intMirror,intCheck,cap-4,compareNumbers)){
         passtest=false;
         cout << "Error 6g: Bug in remove." << endl;
       }
@@ -235,7 +247,8 @@ int main(void){
    			int pick=rand()%(cap-4);
    			intList.remove(intMirror[pick],compareNumbers);
    			removeItem(intMirror,pick,cap-4-i);
-   			if(intList.copyToArray(intCheck,cap) && !cmpArrays(intMirror,intCheck,cap-i-5,compareNumbers)){
+        intList.copyToArray(intCheck,cap);
+   			if(!cmpArrays(intMirror,intCheck,cap-i-5,compareNumbers)){
 	   			passtest=false;
    				cout << "Error 6h:  Bug in remove." << endl;
    			}
@@ -244,11 +257,13 @@ int main(void){
     if(passtest){
       numPassed++;
       cout << "test 7: test assignment operator (deep copy)"  << endl;
-      if(copy.copyToArray(check,20) && !cmpArrays(data,check,20,compareWords)){
+      copy.copyToArray(check,20);
+      if(!cmpArrays(data,check,20,compareWords)){
         passtest=false;
         cout << "Error 7a: Bug in = operator, no deepcopy?"  << endl;
       }
-      if(intCopy.copyToArray(intCheck,cap) && !cmpArrays(intData,intCheck,cap,compareNumbers)){
+      intCopy.copyToArray(intCheck,cap);
+      if(!cmpArrays(intData,intCheck,cap,compareNumbers)){
         passtest=false;
         cout << "Error 7b: Bug in = operator,no deep copy?"  << endl;     
       }
@@ -259,7 +274,8 @@ int main(void){
       int pick[2]={0,19};
       for(int i=0;i<2;i++){
         if(!list.search(data[pick[i]],compareWords)){
-          if(list.copyToArray(check,20) && !cmpArrays(mirror,check,15,compareWords)){
+          list.copyToArray(check,20);
+          if(!cmpArrays(mirror,check,15,compareWords)){
             passtest=false;
             cout << "Error 8a: Bug in search." << endl;
           }
@@ -275,7 +291,8 @@ int main(void){
       int pick[4]={0,1,cap-1,cap-2};
       for(int i=0;i<4;i++){
         if(!intList.search(intData[pick[i]],compareNumbers)){
-          if(intList.copyToArray(intCheck,cap) && !cmpArrays(intMirror,intCheck,cap-104,compareNumbers)){
+          intList.copyToArray(intCheck,cap);
+          if(!cmpArrays(intMirror,intCheck,cap-104,compareNumbers)){
             passtest=false;
             cout << "Error 8b: Bug in search."<< endl;
           }
